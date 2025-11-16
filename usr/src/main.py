3359eee9b3323eb219d35b3cc30ca86d13653c23
Christian <142540893+comdif@users.noreply.github.com>
2025-11-16 16:25:41 +0100

    Update main.py

--- usr/src/main.py
+++ usr/src/main.py
@@ -78,13 +78,6 @@ while True:
 
     if restart_cycle:
         continue
-        
-    # Temperature feature
-    resptemp = urllib2.urlopen('http://192.168.20.116/t')
-    tempout = resptemp.read()
-    resptempin = urllib2.urlopen('http://192.168.20.136/z*Z9/t')
-    tempin = resptempin.read()
-
     
     # Connecting to database
     log.debug("Connecting to database")
